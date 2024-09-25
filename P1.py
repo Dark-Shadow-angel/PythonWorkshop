dbde20b29138803b2e133a539ebd731356da5dcf
Dark-Shadow-angel <sonaligecv04@gmail.com>
2024-09-25 11:09:25 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,25 @@
+'''
+Write a Python program to print the following string in a specific format (see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+Output :
+
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are!  '''
+
+#To print the output by writing code in one line 
+
+print("Twinkle, twinkle, little star", "\n\tHow I wonder what you are!","\n\t\tUp above the world so high,", "\n\t\tLike a diamond in the sky.","\nTwinkle, twinkle, little star,","\n\t How I wonder what you are!")
+
+print("---------------------------------------------------------------")
+#To print output by multiple line of code
+
+print("Twinkle Twinkle, little star,")
+print("\tHow I wonder what you are!")
+print("\t\tUp above the world so high,")
+print("\t\tLike a diamond in the sky.")
+print("Twinkle Twinkle, little star,")
+print("\tHow I wonder what you are!")
