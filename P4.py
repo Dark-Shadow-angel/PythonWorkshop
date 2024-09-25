0fced956bd8088bebc358aebb435b03d4e3098c9
Dark-Shadow-angel <sonaligecv04@gmail.com>
2024-09-25 11:20:57 +0530

    Add files via upload

---
+++ P4.py
@@ -0,0 +1,9 @@
+#Program to calculates area of circle based on the radius entered by the user.
+
+radius=float(input("Enter radius of circle:"))
+area=3.14*radius*radius
+print("Area of circle = ",area)
+
+
+
+
