1496a51d2c2996cf85e84056f218d7c005103798
Dark-Shadow-angel <sonaligecv04@gmail.com>
2024-09-25 12:13:11 +0530

    Add files via upload

---
+++ first_last_color.py
@@ -0,0 +1,24 @@
+"""
+program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+
+"""
+
+color_list = ["Red","Green","White" ,"Black"]
+
+#Printing Color list
+print("List of Colors are :",color_list)
+
+#Printing First color
+print("First color :",color_list[0])
+
+#Printing Last color
+print("Last color :",color_list[-1])
+
+
+
+
+
+#Printing first and last color using string formatting
+print("First color is %s"%(color_list[0]))
+print("Last color is %s"%(color_list[-1]))
