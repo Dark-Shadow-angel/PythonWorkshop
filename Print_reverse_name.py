de6a9a70422996e58cff243b2313cd0c81deae0f
Dark-Shadow-angel <sonaligecv04@gmail.com>
2024-09-25 11:30:45 +0530

    Add files via upload

---
+++ Print_reverse_name.py
@@ -0,0 +1,10 @@
+#program that accepts the user's first and last name and prints them in reverse order with a space between them.
+
+first_name=input("Enter First Name :")
+
+last_name=input("Enter Last Name :")
+
+
+#Display name in reverse order
+
+print(last_name,' ',first_name)
